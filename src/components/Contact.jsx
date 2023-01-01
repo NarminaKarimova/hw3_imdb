957648274ebef0f260cd1f5b8f0953beb7281546
atlashamzali <ahamzali12159@ada.edu.az>
2023-01-01 22:22:58 +0400

    1

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,19 @@
-export default function Contact() {
-    return <div>Contact page</div>;
-  }
-  
+import React, { useState } from 'react'
+
+const Contact = () => {
+    return (
+        <div id="contact-form">
+            <input type="text" placeholder="Your Name" />
+            <br />
+            <input type="email" placeholder="Your email address" />
+            <br />
+            <textarea placeholder="Your message"></textarea>
+            <br />
+            <button>Send Message</button>
+            <br />
+            <span>Thank you for your message, we will be in touch in no time!</span>
+        </div>
+    );
+};
+
+export default Contact;
