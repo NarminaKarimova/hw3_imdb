0df6b7c56760b315ae68a41bc856ea4481c7067f
atlashamzali <ahamzali12159@ada.edu.az>
2023-01-01 19:10:24 +0400

    css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,6 +5,7 @@ import { NavLink } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="nav">
+      <span class="imdb">IMDB</span>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/movies">Movies</NavLink>
       <NavLink to="/contact">Contact</NavLink>
