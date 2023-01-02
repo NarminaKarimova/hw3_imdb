ad3ec9ad31bfefcaa59437ebc7c1bac384868aa6
nurhan <79941449+nurhanaliyev6@users.noreply.github.com>
2023-01-02 15:20:41 +0400

    home page is done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,28 @@
+import "./Home.css";
+
+
 export default function Home() {
-    return <div>Home page</div>;
+  
+    return (
+      <div className="centered" >
+        <center>
+
+
+        <h3>ABOUT OUR TEAM</h3>
+        <br />
+
+        <p >Hello, our dear guest. Welcome to our site created by Narmina Karimova, Atlas Hamzayeva and Nurhan Aliyev ! This site is about showing
+           our projects, Movies from imdb dataset and Contact me pages. You can view our personal
+           websites that contains our projects individually. In a nutshell, we have done personal website, weather app 
+           and digital bussiness card projects during Web And Mobile 1 course.   </p>
+
+          <a href="https://atlashamzali.github.io/hw1/" target="_blank" without rel="noreferrer">Atlas Hamzayeva</a><br />
+          <a href="https://narminakarimova.github.io/Portfolio/" target="_blank" without rel="noreferrer">Narmina Karimova</a> <br />
+          <a href="https://nurhanaliyev6.github.io/My_personal_website/" target="_blank" without rel="noreferrer">Nurhan Aliyev</a><br />
+      
+
+        </center>
+      </div>
+    )
   }
   
