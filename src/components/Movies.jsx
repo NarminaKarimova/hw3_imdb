234f578d630529edde427985777e7768986bd501
NarminaKarimova <97615411+NarminaKarimova@users.noreply.github.com>
2023-01-02 19:32:20 +0400

    Update Movies.jsx

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,4 +1,38 @@
-export default function Movies() {
-    return <div>Movies page</div>;
-  }
-  
+import React, { useEffect, useState } from 'react'
+function Movies() {
+  const [Movies, fetchMovies] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:3500/movie_data')
+      .then((res) => res.json())
+      .then((res) => {
+        fetchMovies(res)
+        console.log(res);
+      })
+  }, []);
+  return (
+    <div className="container">
+      <h1>Reactjs Fetch Api Data from Json Server</h1>
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Movies.map((item, i) => {
+            return <tr>
+              <td>{item.id}</td>
+              <td>{item.Series_Title}</td>
+            </tr>
+          })}
+          <tr>
+
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+export default Movies;
