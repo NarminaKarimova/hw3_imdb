e70dea55c1fe70284e36767b3d9af0664f260bbe
NarminaKarimova <97615411+NarminaKarimova@users.noreply.github.com>
2023-01-03 02:51:00 +0400

    Update Movies.jsx

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,4 +1,5 @@
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useEffect, useState } from 'react'
 function Movies() {
   const [Movies, fetchMovies] = useState([])
@@ -14,12 +15,27 @@ function Movies() {
       })
   }, []);
   return (
-    <div className="container">
-      <h1>Reactjs Fetch Api Data from Json Server</h1>
+    <div className="data">
+      <h1>Movies</h1>
 
-      {Movies.map((item, index) => {
+      {Movies.map((item) => {
         return (
-          <img src={item.Poster_Link}></img>
+          <div className='p-4 d-inline-block justify-content-evenly container' style={{
+            width: 400
+          }}>
+
+            <div className='card' style={{ width: '18rem', padding: 10 }} key={item.id}>
+              <img src={item.Poster_Link} className='card-img-top'></img>
+              <div className="card-body">
+                <h5 className='card-title'>{item.Series_Title}</h5>
+              </div>
+              <span className="genre">
+                <h6>{item.Genre}</h6>
+              </span>
+              <span className="rating">{item.IMDB_Rating}</span>
+            </div>
+          </div>
+
         )
 
       })}
