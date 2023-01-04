1faa366d8daab8666d32799719e49fbf080ccfa3
NarminaKarimova <97615411+NarminaKarimova@users.noreply.github.com>
2023-01-04 20:46:17 +0400

    Update Home.jsx
    editing home jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,47 +7,71 @@ import Nurhan from "./Nurhan.png"
 export default function Home() {
 
   return (
-    <div classNameName="centered">
+    <div className="main">
       <center>
 
-
-        <h3>AGENDA</h3>
         <br />
 
-        <p >Hello, our dear guest. Welcome to our site created by Narmina Karimova, Atlas Hamzayeva and Nurhan Aliyev ! This site is about showing
-          our projects, Movies from imdb dataset and Contact me pages. You can view our personal
-          websites that contains our projects individually. In a nutshell, we have done personal website, weather app
-          and digital bussiness card projects during Web And Mobile 1 course.   </p>
 
-        <div className="container text-center mt-5 mb-2">
-          <h1 className="mb-0">Meet our team</h1>
-        </div>
-        <div className="container mt-3">
-          <div className="row">
-            <div className="col-md-3">
-              <div className="bg-white p-3 text-center rounded box"><img className="img-responsive rounded-circle" src={Atlas} width="90" alt="Atlas"></img>
-                <h5 className="mt-3 name">Atlas Hamzali</h5><span className="work d-block">IT student</span><span className="work d-block">Junior</span>
-                <div className="mt-4 about"><span>Eager to contribute to team success through hard work, attention to detail and excellent organizational skills. Clear understanding of problems and training in programming. &nbsp; Motivated to learn.</span></div>
-                <div className="mt-4">
-                  <a href="https://atlashamzali.github.io/hw1/" target="_blank" without rel="noreferrer">View Profile</a><br />
-                </div>
-              </div>
-            </div>
-            <div className="col-md-3">
-              <div className="bg-white p-3 text-center rounded box"><img className="img-responsive rounded-circle" src={Narmina} width="90" alt="Narmina" ></img>
-                <h5 className="mt-3 name">Narmina Karimova</h5><span className="work d-block">IT student</span><span className="work d-block">Junior</span>
-                <div className="mt-4 about"><span>Nothing much, besides my major in technical field, I am currenly taking a second career path in Business Administration.&nbsp;Trying myself in different fields: from learning UI/UX design to playing a guitar and crocheting.</span></div>
-                <div className="mt-4">
-                  <a href="https://narminakarimova.github.io/Portfolio/" target="_blank" without rel="noreferrer">View Profile</a> <br />
-                </div>
-              </div>
-            </div>
-            <div className="col-md-3">
-              <div className="bg-white p-3 text-center rounded box"><img className="img-responsive rounded-circle" src={Nurhan} width="90" alt="Nurhan"></img>
-                <h5 className="mt-3 name">Nurhan Aliyev</h5><span className="work d-block">CS student</span><span className="work d-block">Senior</span>
-                <div className="mt-4 about"><span>Like to investigate my courses deeply. My passion is about Data Science.&nbsp; I am fond of swimming, walking, praying and smoking</span></div>
-                <div className="mt-4">
-                  <a href="https://nurhanaliyev6.github.io/My_personal_website/" target="_blank" without rel="noreferrer">View Profile</a><br />
+
+        <div className="responsive-container-block outer-container">
+          <div className="responsive-container-block inner-container">
+            <p className="text-blk section-head-text">
+              Our Team
+            </p>
+            <p className="text-blk section-subhead-text">Hello, our dear guest. Welcome to our site created by Narmina Karimova, Atlas Hamzayeva and Nurhan Aliyev ! This site is about showing
+              our projects, Movies from imdb dataset and Contact me pages. You can view our personal
+              websites that contains our projects individually. In a nutshell, we have done personal website, weather app
+              and digital bussiness card projects during Web And Mobile 1 course.
+            </p>
+            <div className="responsive-container-block">
+              <div className="swiper team-swiper">
+                <div className="swiper-wrapper">
+                  <div className="swiper-slide">
+                    <div className="card">
+                      <div className="img-wrapper">
+                        <img src={Atlas} />
+                      </div>
+                      <div className="card-content">
+                        <a className="text-blk name" href="https://atlashamzali.github.io/hw1/" target="_blank" without rel="noreferrer"> Atlas Hamzali
+                        </a>
+                        <p className="text-blk position">Junior IT Student
+                        </p>
+                        <p className="text-blk testimonial">Eager to contribute to team success through hard work, attention to detail and excellent organizational skills. Clear understanding of problems and training in programming. Motivated to learn.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="swiper-slide">
+                    <div className="card">
+                      <div className="img-wrapper">
+                        <img src={Narmina} />
+                      </div>
+                      <div className="card-content">
+                        <a className="text-blk name" href="https://narminakarimova.github.io/Portfolio/" target="_blank" without rel="noreferrer"> Narmina Karimova
+                        </a>
+                        <p className="text-blk position">Junior IT Student
+                        </p>
+                        <p className="text-blk testimonial">Nothing much, besides my major in technical field, I am currenly taking a second career path in Business Administration.Trying myself in different fields: from learning UI/UX design to playing a guitar and crocheting.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="swiper-slide">
+                    <div className="card">
+                      <div className="img-wrapper">
+                        <img src={Nurhan} />
+                      </div>
+                      <div className="card-content">
+                        <a className="text-blk name" href="https://nurhanaliyev6.github.io/My_personal_website/" target="_blank" without rel="noreferrer"> Nurhan Aliyev
+                        </a>
+                        <p className="text-blk position">Senior CS Student
+                        </p>
+                        <p className="text-blk testimonial">Like to investigate my courses deeply. My passion is about Data Science. I am fond of swimming, walking, praying and smoking
+                        </p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -59,10 +83,13 @@ export default function Home() {
 
 
 
+
       </center>
     </div>
   )
 }
+
+
 
 
   
