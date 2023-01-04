ec82e8156fb6bbc36db351db34fe6f3cb3535da1
NarminaKarimova <97615411+NarminaKarimova@users.noreply.github.com>
2023-01-04 18:12:57 +0400

    Update App.jsx
    fixing movie_data link

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import { Route, Routes } from "react-router-dom";
 import Movies from "./components/Movies";
 import Contact from "./components/Contact";
+import DetailedInfo from "./components/DetailedInfo";
 
 export default function App() {
   return (
@@ -12,8 +13,9 @@ export default function App() {
 
       <Routes>
         <Route path="/" element=<Home /> />
-        <Route path="/movies" element=<Movies /> />
+        <Route path="/movie_data" element=<Movies /> />
         <Route path="/contact" element=<Contact /> />
+        <Route path="/movie_data/:id" element=<DetailedInfo /> />
       </Routes>
     </>
   );
