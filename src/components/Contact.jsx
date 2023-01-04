a7f48974617fdf7050964d249c4e2b022d341690
nurhan <79941449+nurhanaliyev6@users.noreply.github.com>
2023-01-04 18:17:32 +0400

    contact me is done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,59 +1,67 @@
-import React, { useState } from 'react'
-import emailjs from '@emailjs/browser';
+import React, { useEffect, useState } from 'react';
 
-const Result =() =>{
-    return(
-        <p>Your message has been successfully sent. We will contact you soon</p>
-    )
-}
-function Contact(){
-    const [result, showResult] = useState(false);
+function Contact() {
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+  const [fullName, setFullName] = useState('');
+  const [phoneNumber, setphoneNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-    emailjs.sendForm('service_mp5porl', 'template_oyfi3vp', e.target, 'user_Y26CewQDlSi598goXD')
-      .then(
-        (result) => {
-          console.log(result.text);
-      }, 
-      (error) => {
-          console.log(error.text);
-      }
-      );
-      e.target.reset();
-      showResult(true);
+
+
+  function sendMessage(formData) {
+    return fetch('http://localhost:3004/messages', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+  }
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const formData = {
+    fullName,
+    phoneNumber,
+    email,
+    message,
   };
+  sendMessage(formData);
+}
 
-    return (
-        <form action="" onSubmit={sendEmail}>
+  return (
+    <center>
+
+    <form onSubmit={handleSubmit}>
+      
         <div className="formWord">
         <h2>Say Hello!</h2>
         <span>Full Name</span>
         <br />
-        <input className ="input100" type="text" name="fullName" required />
+        <input className ="input100" type="text" name="fullName" value={fullName} onChange={(e) => setFullName(e.target.value)} required />
         <br />
         <span>Phone Number</span>
         <br />
-        <input className ="input100" type="text" name="phone" required />
+        <input className ="input100" type="text" name="phone" required value ={phoneNumber} onChange={(e) => setphoneNumber(e.target.value)}/>
         <br />
         <span> Enter Email</span>
         <br />
-        <input className ="input100" type="text" name="email" required />
+        <input className ="input100" type="text" name="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
         <br />
         </div>
         <div className="formWord">
           <span>Message</span>
           <br />
-          <textarea name="message" required></textarea>
+          <textarea name="message" required value={message} onChange={(e) => setMessage(e.target.value)} ></textarea>
           <br />
           <button>SUBMIT</button>
           <div className="row">
-            { result ?  <Result /> : null}
           </div>
         </div>
-        </form>
-    );
-};
+    </form>
+    </center>
+  );
+}
 
 export default Contact;
