b5872451ade7e11172607cb9281b1c16602ade50
NarminaKarimova <97615411+NarminaKarimova@users.noreply.github.com>
2023-01-04 18:10:16 +0400

    Update Movies.jsx
    merging stuff

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,51 +1,47 @@
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Movie from "./Movie"
 import Pagination from "./Pagination"
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import Movie from "./Movie"
 
-const Movies=()=> {
-  const [Movies1, fetchMovies] = useState([])
-  const [loading,setLoading]=useState(false);
-  const [currentPage,SetCurrentPage]=useState(1);
-  const [moviesPerpage]=useState(35);
+
+function Movies() {
+  const [Movies, fetchMovies] = useState([])
+  const [loading] = useState(false);
+  const [currentPage, SetCurrentPage] = useState(1);
+  const [moviesPerpage] = useState(35);
+
   useEffect(() => {
     fetch('http://localhost:3500/movie_data')
       .then((res) => res.json())
       .then((res) => {
-        setLoading(true)
         fetchMovies(res)
-        setLoading(false)
         console.log(res);
       })
       .catch(err => {
-        console.log(err);
+        console.error(err);
       })
   }, []);
 
   // Get current posts
-  const indexOfLastPost=currentPage*moviesPerpage;
-  const indexOfFirstPost=indexOfLastPost-moviesPerpage;
-  const currentPosts=Movies1.slice(indexOfFirstPost,indexOfLastPost)
+  const indexOfLastPost = currentPage * moviesPerpage;
+  const indexOfFirstPost = indexOfLastPost - moviesPerpage;
+  const currentPosts = Movies.slice(indexOfFirstPost, indexOfLastPost)
 
   // Change page
   const paginate = pgNumber => SetCurrentPage(pgNumber);
 
 
 
+
   return (
     <div>
-
-    <h1>Movies</h1>
-
-    <Movie posts={currentPosts} loading={loading} />
-    <Pagination
+      <h1>Movies</h1>
+      <Movie posts={currentPosts} loading={loading} />
+      <Pagination
         postsPerPage={moviesPerpage}
-        totalPosts={Movies1.length}
+        totalPosts={Movies.length}
         paginate={paginate}
       />
     </div>
-
-    )
+  )
 }
 export default Movies;
