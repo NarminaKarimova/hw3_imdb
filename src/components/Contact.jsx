cb2570a7b05511f0801c3416567314aa39432370
NarminaKarimova <97615411+NarminaKarimova@users.noreply.github.com>
2023-01-05 13:08:44 +0400

    changing the container

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
 // import "./Contact.css";
 
 import "./Home.css";
@@ -21,86 +21,112 @@ function Contact() {
     });
   }
 
-function handleSubmit(event) {
-  event.preventDefault();
-  const formData = {
-    fullName,
-    phoneNumber,
-    email,
-    message,
-  };
-  sendMessage(formData);
-}
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = {
+      fullName,
+      phoneNumber,
+      email,
+      message,
+    };
+    sendMessage(formData);
+  }
 
   return (
-    <center style={{width: "400px",
-      height: "450px",
+    <center style={{
+      width: "500px",
+      height: "500px",
       margin: "35px auto",
       border: "1px solid #689eea",
       borderRadius: "10px",
-      backgroundColor: "#89C4E1"}}>
+      backgroundColor: "#89C4E1"
+    }}>
 
-    <form onSubmit={handleSubmit}>
-      
+      <form onSubmit={handleSubmit}>
+
         <div className="contact">
-        <h2 
-        style={{paddingTop: "20px",
-    paddingBottom: "20px",
-    color:"#5053a7"}} 
-    >Say Hello!</h2>
+          <h2
+            style={{
+              paddingTop: "20px",
+              paddingBottom: "20px",
+              color: "#5053a7"
+            }}
+          >Say Hello!</h2>
 
-        <span 
-        style={{color:"#5053a7",
-    fontWeight: "bold"}}
-      >Full Name</span>
-        <br />
-        <input className ="input" type="text" name="fullName" style={{ width:"300px",
-    height: "40px",
-    border: "solid #5053a7",
-    borderRadius: "10px",
-    marginBottom: "10px"}} value={fullName} onChange={(e) => setFullName(e.target.value)} required />
-        <br />
-        <span
-         style={{color:"#5053a7",
-    fontWeight: "bold"}}>Phone Number</span>
-        <br />
-        <input className ="input" type="text" name="phone"  style={{ width:"300px",
-    height: "40px",
-    border: "solid #5053a7",
-    borderRadius: "10px",
-    marginBottom: "10px"}} required value ={phoneNumber} onChange={(e) => setphoneNumber(e.target.value)}/>
-        <br />
-        <span 
-        style={{color:"#5053a7",
-    fontWeight: "bold"}}> Enter Email</span>
-        <br />
-        <input className ="input" type="text" name="email" style={{ width:"300px",
-    height: "40px",
-    border: "solid #5053a7",
-    borderRadius: "10px",
-    marginBottom: "10px"}} required value={email} onChange={(e) => setEmail(e.target.value)} />
-        <br />
-          <span 
-          style={{color:"#5053a7",
-    fontWeight: "bold"}}
-    >Message</span>
+          <span
+            style={{
+              color: "#5053a7",
+              fontWeight: "bold"
+            }}
+          >Full Name</span>
           <br />
-          <textarea name="message" style={{ width:"300px",
-    height: "80px",
-    border: "solid #5053a7",
-    borderRadius: "10px",
-    marginBottom: "10px"}} required value={message} onChange={(e) => setMessage(e.target.value)} ></textarea>
+          <input className="input" type="text" name="fullName" style={{
+            width: "400px",
+            height: "40px",
+            border: "solid #5053a7",
+            borderRadius: "10px",
+            marginBottom: "10px"
+          }} value={fullName} onChange={(e) => setFullName(e.target.value)} required />
           <br />
-          <button style={{marginTop: "3px",
-   backgroundColor: "#8587c3",
-   border: "solid #5053a7",
-   borderRadius: "7px"}}>Submit</button>
+          <span
+            style={{
+              color: "#5053a7",
+              fontWeight: "bold"
+            }}>Phone Number</span>
+          <br />
+          <input className="input" type="text" name="phone" style={{
+            width: "400px",
+            height: "40px",
+            border: "solid #5053a7",
+            borderRadius: "10px",
+            marginBottom: "10px"
+          }} required value={phoneNumber} onChange={(e) => setphoneNumber(e.target.value)} />
+          <br />
+          <span
+            style={{
+              color: "#5053a7",
+              fontWeight: "bold"
+            }}> Enter Email</span>
+          <br />
+          <input className="input" type="text" name="email" style={{
+            width: "400px",
+            height: "40px",
+            border: "solid #5053a7",
+            borderRadius: "10px",
+            marginBottom: "10px"
+          }} required value={email} onChange={(e) => setEmail(e.target.value)} />
+          <br />
+          <span
+            style={{
+              color: "#5053a7",
+              fontWeight: "bold"
+            }}
+          >Message</span>
+          <br />
+          <textarea name="message" style={{
+            width: "400px",
+            height: "80px",
+            border: "solid #5053a7",
+            borderRadius: "10px",
+            marginBottom: "10px"
+          }} required value={message} onChange={(e) => setMessage(e.target.value)} ></textarea>
+          <br />
+          <button style={{
+            marginTop: "3px",
+            backgroundColor: "#8587c3",
+            border: "solid #5053a7",
+            borderRadius: "7px",
+            width: "100px",
+            height: "45px"
+
+          }}>Submit</button>
           <div className="row">
           </div>
         </div>
-    </form>
+      </form>
     </center>
   );
 }
 
 export default Contact;
+
